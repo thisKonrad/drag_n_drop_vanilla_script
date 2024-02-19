cb1de0ff4cb42d9385032a1196678f22e4621622
this.Konrad <konradlebt@gmail.com>
2024-02-19 16:58:31 +0100

    draggable funtionality done with out flexible position dragged items

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 /* :::: main.js :::: */
 const draggable = document.querySelectorAll('[data-theme="drag"]');
+const dropZone = document.querySelectorAll('[data-theme="drop"]');
+
+draggable.forEach((item) => {
+    item.addEventListener('dragstart', () =>
+        item.classList.add('dragging')
+    )
+});
+
+draggable.forEach((item) => {
+    item.addEventListener('dragend', () =>
+        item.classList.remove('dragging')
+    )
+});
+
+/* :::: drop zone :::: */
+dropZone.forEach((item) => {
+    item.addEventListener('dragover', () => {
+        console.log('over');
+        const dragged = document.querySelector('.dragging');
+        item.appendChild(dragged)
+    })
+});
