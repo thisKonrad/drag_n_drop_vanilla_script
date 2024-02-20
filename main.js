569986aad9f34f5d9b103c3bd3c51e7d790df566
this.Konrad <konradlebt@gmail.com>
2024-02-20 09:38:26 +0100

    vanilla drag n drop runs

--- main.js
+++ main.js
@@ -16,13 +16,16 @@ draggable.forEach((item) => {
 
 /* :::: drop zone :::: */
 dropZone.forEach((target) => {
+
     target.addEventListener('dragover', (event) => {
+
         event.preventDefault();
 
         const afterElement = getCurrentDraggedItem(target, event.clientY);
+
         const dragged = document.querySelector('.dragging');
         /* when element if over no other element:*/
-        if (afterElement === null) {
+        if (afterElement == null) {
             target.appendChild(dragged)
         }
         else {
@@ -41,18 +44,17 @@ function getCurrentDraggedItem(dropZone, yPosition) {
     /* :: reduce starts from closest item to next till 
     * (infinity) = border of the pregiven Max.Value
     * and then append a child => the dragged item: */
-    currentDraggedItem.reduce((closest, child) => {
+    return currentDraggedItem.reduce((closest, child) => {
 
         /*the size of an element and its position relative to the viewport*/
         const boundingBox = child.getBoundingClientRect();
 
-        const offset = yPosition - boundingBox.top - boundingBox.height / 2;
+        const offset = yPosition - boundingBox.top - (boundingBox.height / 2);
 
         console.log('center box offset: ', offset);
-
         console.log('box: ', boundingBox);
 
-        if (offset > 0 && offset > closest.offset) {
+        if (offset < 0 && offset > closest.offset) {
 
             return { offset: offset, element: child };
 
